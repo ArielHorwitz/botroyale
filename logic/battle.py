c18222bc28df8fc153d9b5f86b1332799bfc28c6
Moral <sn@k.e>
2022-06-18 14:53:53 +0300

    turn order by previous turn's ap usage + minor bug fix (sometimes ap lost without moving)

--- logic/battle.py
+++ logic/battle.py
@@ -41,6 +41,7 @@ class Battle(BaseLogicAPI):
         self.turn_count = 0
         self.round_count = 0
         self.ap = np.zeros(self.num_of_bots)
+        self.round_ap_spent = np.zeros(self.num_of_bots)
         # walls and pits are sequences of 2D coordinates (a 2-sequence)
         self.axis_size, self.walls, self.pits = random_map()
         self.map_size = int(self.axis_size), int(self.axis_size)
@@ -59,19 +60,28 @@ class Battle(BaseLogicAPI):
         self._apply_diff(bot_id, diff, ap_spent)
 
     def _next_round(self):
-        bots_id = np.arange(self.num_of_bots)[self.alive_mask]
-        self.round_remaining_turns = list(bots_id)
-        random.shuffle(self.round_remaining_turns)
+        self._next_round_order()
+        self.round_ap_spent = np.zeros(self.num_of_bots)
         self.ap[self.alive_mask] += 50
         self.ap[self.ap > 100] = 100
         self.round_count += 1
+
+    def _next_round_order(self):
+        bots_id = np.arange(self.num_of_bots)
+        p = RNG.permutation(bots_id[self.alive_mask])
+        while len(p) > 0:
+            min_index = np.argmin(self.round_ap_spent[p][self.alive_mask[p]])
+            self.round_remaining_turns.append(p[min_index])
+            p = np.delete(p, min_index)
 
     def _get_bot_move(self, bot_id):
         diff = np.zeros((self.num_of_bots, 2), dtype='int8')
         move_diff = self.bots[bot_id].get_move()
         ap_spent = self._calc_ap(move_diff)
-        if self._check_legal_move(bot_id, move_diff, self.positions[bot_id], ap_spent):
+        if self._check_legal_move(bot_id, move_diff, ap_spent):
             diff[bot_id] += move_diff
+        else:
+            ap_spent = 0
         return diff, ap_spent
 
     def _calc_ap(self, diff):
@@ -80,12 +90,15 @@ class Battle(BaseLogicAPI):
         else:
             return 0
 
-    def _check_legal_move(self, bot_id, diff, position, spent_ap):
+    def _check_legal_move(self, bot_id, diff, spent_ap):
+        position = self.positions[bot_id]
         if self.ap[bot_id] - spent_ap < 0:
             return False
         new_position = position + diff
+        # check if moving out of bounds
         if np.sum(new_position < 0) or np.sum(new_position > self.axis_size - 1):
             return False
+        # check if moving to a wall tile
         if ((self.walls == new_position).sum(axis=1) >= 2).sum() > 0:
             return False
         return True
@@ -94,6 +107,7 @@ class Battle(BaseLogicAPI):
         self.positions += diff
         self._apply_mortality()
         self.ap[bot_id] -= ap_spent
+        self.round_ap_spent[bot_id] += ap_spent
         self.turn_count += 1
         self.history.append(diff)
 
@@ -125,7 +139,7 @@ class Battle(BaseLogicAPI):
             winner_str = ''
             if self.alive_mask.sum() == 1:
                 winner = np.arange(self.num_of_bots)[self.alive_mask]
-                winner_str = f'\n\n This game winner is: {winner}'
+                winner_str = f'\n\n This game winner is: unit #{winner[0]}'
             state_str = 'GAME OVER\n\n' + state_str + winner_str
         return state_str
 
