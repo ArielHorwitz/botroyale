21f5ee81431c369d928ce042eba569c680c4846b
imcrazeegamer <imcrazeegamer@gmail.com>
2022-06-22 16:52:00 +0300

    moved the code around to the correct locations

--- api/bot_api.py
+++ api/bot_api.py
@@ -1,3 +1,17 @@
+import numpy as np
+
+
+class Direction:
+    N = np.asarray([1, 0])
+    S = np.asarray([-1, 0])
+    E = np.asarray([0, 1])
+    W = np.asarray([0, -1])
+    NW = np.asarray([1, -1])
+    NE = np.asarray([1, 1])
+    SW = np.asarray([-1, -1])
+    SE = np.asarray([-1, 1])
+    HOLD = np.asarray([0, 0])
+
 
 class WorldInfo:
     round = 0
@@ -8,3 +22,27 @@ class WorldInfo:
     items = []
     units = []
     ap = []
+
+
+class BaseBot:
+    DIRECTIONS = [
+        Direction.N,
+        Direction.S,
+        Direction.E,
+        Direction.W,
+        Direction.NW,
+        Direction.NE,
+        Direction.SW,
+        Direction.SE,
+        Direction.HOLD]
+
+    def __init__(self, id: int):
+        self.id = id
+
+    def get_move(self, world: WorldInfo = None):
+        """
+        Called by the Game Logic
+        :param world: state of world
+        :return: action to take
+        """
+        return Direction.HOLD
--- bots/__init__.py
+++ bots/__init__.py
@@ -1,46 +1,9 @@
 import random
 
-import numpy as np
-from api.bot_api import WorldInfo
+from api.bot_api import BaseBot
 from pkgutil import iter_modules
 from pathlib import Path
 from importlib import import_module
-
-
-class Direction:
-    N = np.asarray([1, 0])
-    S = np.asarray([-1, 0])
-    E = np.asarray([0, 1])
-    W = np.asarray([0, -1])
-    NW = np.asarray([1, -1])
-    NE = np.asarray([1, 1])
-    SW = np.asarray([-1, -1])
-    SE = np.asarray([-1, 1])
-    HOLD = np.asarray([0, 0])
-
-
-class BaseBot:
-    DIRECTIONS = [
-        Direction.N,
-        Direction.S,
-        Direction.E,
-        Direction.W,
-        Direction.NW,
-        Direction.NE,
-        Direction.SW,
-        Direction.SE,
-        Direction.HOLD]
-
-    def __init__(self, id: int):
-        self.id = id
-
-    def get_move(self, world: WorldInfo = None):
-        """
-        Called by the Game Logic
-        :param world: state of world
-        :return: action to take
-        """
-        return Direction.HOLD
 
 
 def bot_importer():
