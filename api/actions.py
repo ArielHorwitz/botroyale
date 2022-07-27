ecad8392b896511c5d65723e82127e24aeeec273
imcrazeegamer <imcrazeegamer@gmail.com>
2022-07-27 13:04:34 +0300

    action balance and crazee bot crash fix

--- api/actions.py
+++ api/actions.py
@@ -6,32 +6,34 @@ class Action:
 
 
 class Move(Action):
-    ap = 10
+    ap = 20
 
     def __init__(self, target_tile):
         assert is_hex(target_tile)
         self.target = target_tile
 
     def __repr__(self):
-        return f'<Move action: {self.target.x}, {self.target.y}>'
+        return f'<Move: {self.target.x}, {self.target.y}>'
 
 
 class Push(Move):
     ap = 30
 
     def __repr__(self):
-        return f'<Push action: {self.target.x}, {self.target.y}>'
+        return f'<Push: {self.target.x}, {self.target.y}>'
 
 
 class Jump(Move):
-    ap = 40
+    ap = 45
 
     def __repr__(self):
-        return f'<Jump action: {self.target.x}, {self.target.y}>'
+        return f'<Jump: {self.target.x}, {self.target.y}>'
 
 
 class Idle(Action):
 
     def __repr__(self):
-        return f'<Idle action>'
+        return f'<Idle>'
 
+
+ALL_ACTIONS = (Idle, Move, Push, Jump)
