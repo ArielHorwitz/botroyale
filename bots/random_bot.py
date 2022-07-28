e15855b88ec28468d4c541844a2f876bb49dde8c
Ariel Horwitz <_@_._>
2022-07-28 15:17:37 +0300

    Update RandomBot

--- bots/random_bot.py
+++ bots/random_bot.py
@@ -1,32 +1,28 @@
+# Maintainer: ninja
 import random
-from bots import BaseBot
-from util.hexagon import Hex
+import time
+from api.bots import BaseBot, center_distance
 from api.actions import Move, Push, Jump
 
 
 class RandomBot(BaseBot):
-    NAME = 'RandomBot'
+    NAME = 'random'
     SPRITE = 'ellipse'
     TESTING_ONLY = True
-
-    def __init__(self, id):
-        super().__init__(id)
 
     def poll_action(self, state):
         pos = state.positions[self.id]
         action = self.get_target(pos)
         attempts = 5
         while not self.safe_target(action.target, state) and attempts:
-            self.logger(f'Unsafe target {action}, finding new target (remaining attemps: {attempts})')
             action = self.get_target(pos)
             attempts -= 1
-        self.logger(f'Found safe target: {action}')
         return action
 
     def safe_target(self, target, state):
         return all([
             target not in state.pits,
-            target.get_distance(Hex(0, 0)) < state.death_radius,
+            center_distance(target) < state.death_radius,
             ])
 
     def get_target(self, pos):
@@ -36,4 +32,19 @@ class RandomBot(BaseBot):
         ])
 
 
-BOT = RandomBot
+class SleeperBot(RandomBot):
+    NAME = "sleeper"
+    TESTING_ONLY = True
+    sleep_time = 3
+
+    def poll_action(self, state):
+        time.sleep(self.sleep_time)
+        return super().poll_action(state)
+
+
+class SnoozerBot(SleeperBot):
+    NAME = 'snoozer'
+    sleep_time = 1
+
+
+BOTS = [RandomBot, SleeperBot, SnoozerBot]
