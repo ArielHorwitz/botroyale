069c9b8f7b91f53cb13fa49180f7b0291a9ef655
Ariel Horwitz <_@_._>
2022-08-11 13:16:29 +0300

    Add options to default get_bot_classes
    Add type hints and docstrings to bots.__init__ module

--- bots/__init__.py
+++ bots/__init__.py
@@ -1,24 +1,108 @@
+from typing import Optional, Sequence, Callable
 import random
-
-from api.logging import logger
-from api.bots import BaseBot
 from pkgutil import iter_modules
 from importlib import import_module
-
 from util import PROJ_DIR
-from util.settings import Settings
+from api.bots import BaseBot
 
 
 BOTS_DIR = PROJ_DIR / 'bots'
 
 
-def bot_importer():
+class NotFairError(Exception):
+    pass
+
+
+def get_bot_classes(
+        total_slots: int,
+        selection: Optional[Sequence[str]] = None,
+        ignore: Optional[Sequence[str]] = None,
+        include_testing: bool = False,
+        keep_fair: bool = True,
+        no_dummies: bool = False,
+        all_play: bool = False,
+        ) -> list[type]:
+    """Returns a list of bot classes.
+
+    total_slots         -- number of bots to return
+    selection           -- list of bot names to select from (will select from all bots if no selection provided)
+    ignore              -- list of bot names to remove from selection
+    include_testing     -- include bots marked as "testing only"
+    keep_fair           -- ensure an equal number of slots for each bot, filling remaining slots with dummy bots
+    no_dummies          -- when keep_fair is true, ensure that no dummy bots are added
+    all_play            -- ensure that every bot is selected at least once
     """
-    Imports all bots from modules in package
+    available_bots = set(BOTS.keys())
+    # Filter bots
+    if selection is not None:
+        available_bots &= set(selection)
+    if ignore is not None:
+        available_bots -= set(ignore)
+    if not include_testing:
+        testing_only = {b for b in available_bots if BOTS[b].TESTING_ONLY}
+        available_bots -= testing_only
+
+    # Count slots
+    total_bots = len(available_bots)
+    if total_bots == 0:
+        raise NotFairError(f'Found 0 bots to fill {total_slots} slots. Selection: {selection} - {ignore} (minus testing: {not include_testing})')
+    slots_per_bot = int(total_slots / total_bots)
+
+
+    # Fill slots
+    if slots_per_bot < 1:
+        # We have more bots than slots - choose a random sample, size of total_slots
+        if all_play:
+            raise NotFairError(f'Requested for all {total_bots} bots to play, but only {total_slots} slots available.')
+        selected_bots = list(available_bots)
+        random.shuffle(selected_bots)
+        selected_bots = selected_bots[:total_slots]
+    else:
+        # We have enough slots for all bots
+        selected_bots = list(available_bots) * slots_per_bot
+        remaining_slots = total_slots - len(selected_bots)
+        if remaining_slots:
+            if keep_fair and no_dummies:
+                raise NotFairError(f'Requested to keep fair and no dummies, but we have {total_slots} total slots for {len(selected_bots)} bots ({remaining_slots} slots remainder).')
+            elif keep_fair:
+                # Fill with dummies
+                fill = ['dummy'] * remaining_slots
+            else:
+                # Fill with as many different bots as we can fit
+                fill = list(available_bots)
+                random.shuffle(fill)
+                fill = fill[:remaining_slots]
+            selected_bots.extend(fill)
+
+    # Convert to classes
+    bot_classes = []
+    for bot_name in selected_bots:
+        bot_classes.append(BOTS[bot_name])
+    random.shuffle(bot_classes)
+
+    assert len(bot_classes) == total_slots
+    return bot_classes
+
+
+def bot_getter(**kwargs) -> Callable[[int], list[type]]:
+    """Returns a callable to `get_bot_classes` with preconfigued kwargs.
+    The statement `bot_getter()` (the return value) is functionally equivalent
+    to `get_bot_classes` (the function name).
+
+    Example usage:
+
+    `bot_getter = get_bots(selection=['random', 'idle'], include_testing=True)`
+    `Battle(bot_classes_getter=bot_getter)`
     """
+    def bot_getter_inner(slots: int) -> list[type]:
+        return get_bot_classes(slots, **kwargs)
+    return bot_getter_inner
+
+
+def _bot_importer(get_legend: bool = False) -> dict[str, type]:
+    """Imports and returns bot names and classes from modules in the bots package."""
     bots = {}
-    logger('Available bots:\n(Legend: » competitive - test)')
-    for (_, module_name, _) in iter_modules([str(BOTS_DIR)]):
+    for (finder, module_name, ispkg) in iter_modules([str(BOTS_DIR)]):
         module = import_module(f"{__name__}.{module_name}")
         if hasattr(module, "BOT"):
             module_bots = [getattr(module, "BOT")]
@@ -31,33 +115,8 @@ def bot_importer():
             if bot.NAME in bots:
                 raise KeyError(f'Bot name: "{bot.NAME}" (from module: {module_name}) already in use.')
             bots[bot.NAME] = bot
-            prefix = '-' if bot.TESTING_ONLY else '»'
-            logger(f'{prefix} {bot.NAME:<20} | from module: {module_name:<20}')
     return bots
 
 
-BOT_REQ = Settings.get("bots.bot_names", [])
-BOTS_IGNORED = Settings.get("bots.bot_names_ignore", [])
-BOTS = bot_importer()
-
-
-def get_bot_classes(num_of_bots: int) -> list[type]:
-    """
-    Returns a list of bot classes from BOTS as configured in settings.
-
-    :param num_of_bots:     number of bots classes to collect
-    :return:                list of bot classes
-    """
-    if len(BOTS) == 0:
-        game_classes = [BaseBot] * num_of_bots
-    else:
-        game_classes = [BOTS[req] for req in BOT_REQ if req in BOTS]
-        non_testing_bots = [bot for bot in BOTS.values() if not bot.TESTING_ONLY and bot.NAME not in BOTS_IGNORED]
-        random.shuffle(non_testing_bots)
-        while len(game_classes) < num_of_bots:
-            idx = (num_of_bots - len(game_classes)) % len(non_testing_bots)
-            game_classes.append(non_testing_bots[idx])
-    game_classes = game_classes[:num_of_bots]
-    logger('Selected bots:')
-    logger('\n'.join(f'#{i:<2} {cls.NAME}' for i, cls in enumerate(game_classes)))
-    return game_classes
+BOTS = _bot_importer()
+assert 'dummy' in BOTS  # There should always be at least one bot, let it be a dummy
