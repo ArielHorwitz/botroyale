a6cf2ae1f1ae8c050294cf4e091ff4d7a4729227
Ariel Horwitz <_@_._>
2022-08-24 11:46:49 +0300

    Add preview gif

--- __init__.py
+++ __init__.py
@@ -8,15 +8,29 @@ from util.file import file_load, file_dump
 def _generate_fixed_readme():
     """Generate a "fixed" README for the html docs.
 
-    Insert version and a dynamically generated reference to the guides that are
-    not relevant for the main project's static README."""
+    We want to use the readme for our docs index page. While the readme is
+    designed to work well for Github, not so much for the html docs. A "fixed"
+    readme will be generated in the docs folder (hence the
+    `.. include:: ./docs/README.md` above). Since the fixed readme has a
+    difference location, the relative links also need to be fixed. And so the
+    following is done before writing the fixed readme:
+
+    - Insert version
+    - Insert absolute link to the guides
+    - Fix assets folder relative link to absolute link
+    """
+    readme_path = PROJ_DIR / 'README.md'
+    fixed_readme_path = PROJ_DIR / 'docs' / 'README.md'
+    # Add version and link to guides up top
     added_lines = '\n'.join([
         f'Documentation built on `v{VERSION}`.\n',
         f'Check out `{PROJ_DIR.name}.guides` for many resources, from beginner to advanced.\n'
     ])
-    readme_path = PROJ_DIR / 'README.md'
-    fixed_readme_path = PROJ_DIR / 'docs' / 'README.md'
     fixed_readme = f'{added_lines}\n{file_load(readme_path)}'
+    # Fix the assets path for the preview gif
+    fixed_assets_path = PROJ_DIR / 'assets'
+    oldstr, newstr = 'assets/', f'{str(fixed_assets_path)}/'
+    fixed_readme = fixed_readme.replace(oldstr, newstr)
     file_dump(fixed_readme_path, fixed_readme)
 
 
