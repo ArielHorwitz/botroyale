c59627bd7205ec62493aebfe9513a6d3e7b97ff2
Ariel Horwitz <_@_._>
2022-08-24 19:38:31 +0300

    Rearrange main menu

--- gui/game/game.py
+++ gui/game/game.py
@@ -8,47 +8,56 @@ class GameScreen(widgets.AnchorLayout):
         assert isinstance(api, GameAPI)
         self.api = api
         self.new_battle = None
+        self.input_widgets = {}
         # Make widgets
-        main_frame = self.add(widgets.BoxLayout(orientation='vertical'))
-        title = widgets.Label(text=api.get_menu_title())
-        input_frame = widgets.StackLayout(orientation='tb-lr')
+        main_frame = self.add(widgets.BoxLayout())
+        info_panel = widgets.Label(text=api.get_menu_title())
+        self.input_widgets_container = widgets.StackLayout(orientation='tb-lr')
         new_battle_btn = widgets.Button(
             text=f'Start New Battle ([i]spacebar[/i])',
             on_release=self.set_new_battle, markup=True)
         # Assemble
-        title_frame = main_frame.add(widgets.AnchorLayout())
-        title_frame.make_bg(PALETTE_BG[4])
-        title_frame.set_size(y=100)
-        title_frame.add(title).set_size(hx=0.5, hy=0.5)
-        input_outer_frame = main_frame.add(widgets.AnchorLayout())
-        input_outer_frame.add(input_frame).set_size(hx=0.9, hy=0.9)
-        input_outer_frame.make_bg(PALETTE_BG[3])
-        new_battle_frame = main_frame.add(widgets.AnchorLayout())
+        left_panel = main_frame.add(widgets.BoxLayout(orientation='vertical'))
+        left_panel.make_bg(PALETTE_BG[4])
+        left_panel.set_size(x=450)
+        # Info panel frame
+        info_panel_frame = left_panel.add(widgets.AnchorLayout())
+        info_panel_frame.add(info_panel).set_size(hx=0.5, hy=0.5)
+        # New battle frame
+        new_battle_frame = left_panel.add(widgets.AnchorLayout())
         new_battle_frame.set_size(y=100)
-        new_battle_frame.make_bg(PALETTE_BG[1])
-        new_battle_frame.add(new_battle_btn).set_size(hx=0.5, hy=0.5)
+        new_battle_frame.add(new_battle_btn).set_size(hx=0.8, hy=0.5)
+        # Input frame
+        input_widgets_frame = main_frame.add(widgets.AnchorLayout())
+        input_widgets_frame.add(self.input_widgets_container).set_size(hx=0.9, hy=0.9)
+        input_widgets_frame.make_bg(PALETTE_BG[3])
         # Add input widgets
+        initial_menu_widgets = self.api.get_menu_widgets()
+        self.remake_input_widgets(menu_widgets=initial_menu_widgets)
+
+    def remake_input_widgets(self, menu_widgets):
         self.input_widgets = {}
-        for iw in api.get_menu_widgets():
+        self.input_widgets_container.clear_widgets()
+        for iw in menu_widgets:
             container = self.add_input_widget(iw)
-            input_frame.add(container)
+            self.input_widgets_container.add(container)
 
     def add_input_widget(self, iw):
         container = widgets.BoxLayout(orientation='vertical')
-        half_size = 40
-        container_size = half_size * 2
+        width = 250
+        container_size = 40
         if iw.type == 'spacer':
             container.make_bg(PALETTE_BG[0])
             container.add(widgets.Label(text=iw.label))
-            container.set_size(hx=0.25, y=container_size)
+            container.set_size(x=width, y=container_size)
             return container
         if iw.type == 'toggle':
-            container_size = half_size
             w = widgets.ToggleButton(text=iw.label)
             w.active = iw.default
             container.add(w)
             value_getter = lambda: w.active
         elif iw.type == 'text':
+            container_size *= 2
             w = widgets.Entry()
             container.add(widgets.Label(text=iw.label))
             container.add(w)
@@ -56,7 +65,6 @@ class GameScreen(widgets.AnchorLayout):
             value_getter = lambda: w.text
         elif iw.type == 'select':
             container.orientation = 'horizontal'
-            container_size = half_size
             w = widgets.DropDownSelect(callback=lambda *a: None)
             if iw.options is None:
                 raise ValueError(f'Cannot make a select InputWidget without options')
@@ -66,15 +74,16 @@ class GameScreen(widgets.AnchorLayout):
             container.add(w)
             value_getter = lambda: w.text
         elif iw.type == 'slider':
+            container_size *= 2
             w = widgets.SliderText()
-            w.value = iw.default
+            w.slider.value = iw.default
             container.add(widgets.Label(text=iw.label))
             container.add(w)
             value_getter = lambda: w.slider.value
         else:
             raise ValueError(f'Unknown InputWidget type: {iw.type}')
         self.input_widgets[iw.sendto] = value_getter
-        container.set_size(hx=0.25, y=container_size)
+        container.set_size(x=width, y=container_size)
         return container
 
     def set_new_battle(self, *args):
