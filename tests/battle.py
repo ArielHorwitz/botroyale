2e870056edd098ee9e700f012a46c26a83b123fe
Ariel Horwitz <ArielHorwitz@users.noreply.github.com>
2022-09-14 23:38:26 +0300

    Add Battle tests

--- tests/battle.py
+++ tests/battle.py
@@ -7,13 +7,15 @@ from botroyale.logic.battle import Battle
 from tests.maps import st_map
 
 
+BOT_SELECTION = BotSelection()
 BOT_SELECTION_BASIC = BotSelection(["basic"])
 
 
-def new_battle(map):
+def new_battle(map="basic", bots=BOT_SELECTION_BASIC, **kwargs):
     return Battle(
         initial_state=get_map_state(map),
-        bots=BOT_SELECTION_BASIC,
+        bots=bots,
+        **kwargs,
         enable_logging=False,
     )
 
@@ -21,9 +23,15 @@ def new_battle(map):
 @given(st_map)
 @settings(deadline=None)
 def test_make_battle(map):
+    Battle(enable_logging=False)
+
+
+@given(st_map)
+@settings(deadline=None)
+def test_make_battle_maps(map):
     new_battle(map)
 
 
 def test_play_battle():
-    b = new_battle("basic")
+    b = new_battle()
     b.play_all()
